4b43042326bbe644ccfda3c81a138375321808ed
BQZic <iceyzou99@gmail.com>
2021-05-08 14:44:56 -0400

    Diff between images

--- bp_test.py
+++ bp_test.py
@@ -10,6 +10,33 @@ Birthday Paradox Test
 
 import os, random
 import glob
+from PIL import Image
+from collections import Counter
+import numpy as np
+
+def L2Norm(H1,H2):
+    distance =0
+    for i in range(len(H1)):
+        distance += np.square(H1[i]-H2[i])
+    return np.sqrt(distance)
+
+def convertImage(img1):
+    reference_image_1 = Image.open(img1)
+    reference_image_arr = np.asarray(reference_image_1)
+    flat_array_1 = reference_image_arr.flatten()
+    RH1 = Counter(flat_array_1)
+    H1 = []
+    for i in range(256):
+        if i in RH1.keys():
+            H1.append(RH1[i])
+        else:
+            H1.append(0)
+    return H1
+
+def compareImages(img1, img2):
+    H1 = convertImage(img1)
+    H2 = convertImage(img2)
+    return L2Norm(H1, H2)
 
 NUM_SAMPLED_IMAGES = 20
 FILE_NAME = "/content/drive/MyDrive/Data/Test3/*.jpg"
@@ -17,4 +44,4 @@ FILE_NAME = "/content/drive/MyDrive/Data/Test3/*.jpg"
 selected_images = random.sample(glob.glob(FILE_NAME), NUM_SAMPLED_IMAGES)
 
 for image in enumerate(selected_images, 1):
-    continue
+    print(image)
